25fb8ffd74601818c15351114b8b970c1ad18370
jollysully <127195594+jollysully@users.noreply.github.com>
2023-03-21 14:00:31 +0000

    db query modification
    refined save grade sql query and edited database to accept grades

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "113e14ce0b8ee09d"; }
+/******/ 		__webpack_require__.h = function() { return "cfec62e07df3f149"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/api/saveGrade.js
+++ pages/api/saveGrade.js
@@ -9,22 +9,30 @@ export default function SaveGrade(req, res) {
     const mysql = require('mysql2');
 
       // create the connection to database
-    const connection = mysql.createConnection({
+      const connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'example',
-        port: 2222,
+        password: 'password12',
+        port: 3306,
         database: 'wse'
     });
 
 
     connection.query(
-        "INSERT INTO wse.grades (sid,courseid,grade) VALUES ('"+sid+"','"+cid+"','"+grade+"');",
+        "INSERT INTO grades (sid,courseid,grade) VALUES ('"+sid+"','"+cid+"','"+grade+"');",
         function(err, results, fields) {
+            
+            //error handler
+            if (err) {
+                console.log(err);
+                res.status(200).json("fail" + err.sqlMessage);
+              return
+              }
 
+              else {res.status(200).json("ok");}
 
-            // return back the records
-            res.status(200).json(results);
+            //console.log(results);
+            //res.status(200).json(results);
 
         }
     );
